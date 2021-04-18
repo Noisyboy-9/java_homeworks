9605a0b53d69939af6da4cdd953b977b213149f5
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-04-18 20:53:10 +0430

    Add javadoc for project 3 question 3

--- project_3/question_3/src/sina/shariati/stack/Stack.java
+++ project_3/question_3/src/sina/shariati/stack/Stack.java
@@ -3,11 +3,19 @@ package sina.shariati.stack;
 import sina.shariati.exceptions.StackUnderFlowException;
 
 
+/**
+ * The type Stack.
+ */
 public class Stack {
     private final int size;
     private final StackItem[] stackArray;
     private int topIndex;
 
+    /**
+     * Instantiates a new Stack.
+     *
+     * @param size the size
+     */
     public Stack(int size) {
         this.size = size;
         this.stackArray = new StackItem[size];
@@ -25,14 +33,29 @@ public class Stack {
         return test;
     }
 
+    /**
+     * Check to see if stack is full.
+     *
+     * @return the boolean
+     */
     public boolean isFull() {
         return this.topIndex == this.size - 1;
     }
 
+    /**
+     * Check to see if stack is empty.
+     *
+     * @return the boolean
+     */
     public boolean isEmpty() {
         return this.topIndex == -1;
     }
 
+    /**
+     * Push a item to the stack.
+     *
+     * @param item the item
+     */
     public void push(StackItem item) {
         if (this.isFull()) {
             throw new StackOverflowError("stack is full");
@@ -43,6 +66,12 @@ public class Stack {
         this.topIndex = newTopIndex;
     }
 
+    /**
+     * Pop a item from stack.
+     *
+     * @return the stack item
+     * @throws StackUnderFlowException the stack under flow exception
+     */
     public StackItem pop() throws StackUnderFlowException {
         if (this.isEmpty()) {
             throw new StackUnderFlowException("stack is empty");
@@ -51,6 +80,12 @@ public class Stack {
         return this.stackArray[this.topIndex--];
     }
 
+    /**
+     * See first item of the stack.
+     *
+     * @return the stack item
+     * @throws StackUnderFlowException the stack under flow exception
+     */
     public StackItem peek() throws StackUnderFlowException {
         if (this.isEmpty()) {
             throw new StackUnderFlowException("stack is empty");
--- project_3/question_3/src/sina/shariati/stack/StackItem.java
+++ project_3/question_3/src/sina/shariati/stack/StackItem.java
@@ -1,12 +1,25 @@
 package sina.shariati.stack;
 
+/**
+ * The type Stack item.
+ */
 public class StackItem {
     private final int value;
 
+    /**
+     * Instantiates a new Stack item.
+     *
+     * @param value the value
+     */
     public StackItem(int value) {
         this.value = value;
     }
 
+    /**
+     * Gets value.
+     *
+     * @return the value
+     */
     public int getValue() {
         return value;
     }
